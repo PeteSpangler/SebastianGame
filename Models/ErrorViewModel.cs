3f727165a091cbd1b0650e51ef94bad36521523f
Peter Spangler <petertspangler@outlook.com>
2023-02-28 08:46:15 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SebastianGame.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
